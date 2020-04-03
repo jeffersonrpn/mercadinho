0f56239eacde097a58d247df4a503136b7304e43
Jefferson Neves <jefferson.rpn@gmail.com>
2020-04-03 11:58:52 -0300

    Cria servidor web mínimo

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const forceSsl = require('force-ssl-heroku');
+
+const app = express();
+app.use(forceSsl);
+
+app.use(express.static("dist"));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "dist", "index.html"));
+});
+
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => console.log(`Server running on ${port}`));
