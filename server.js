ec5038d823c9dd6d742d2fdde56ccdc9433c2c1a
Jefferson Neves <jefferson.rpn@gmail.com>
2020-05-05 13:34:17 -0300

    Reverte alteração do SSL para o heroku

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require("express");
-// const forceSsl = require('force-ssl-heroku');
+const forceSsl = require('force-ssl-heroku');
 const path = require("path");
 
 const app = express();
-// app.use(forceSsl);
+app.use(forceSsl);
 
 app.use(express.static("dist"));
 
